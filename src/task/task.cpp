9821261609a0f0d81f12aabd116cf8581bc435db
Vladz <hramzik.ru@yandex.ru>
2023-10-08 01:20:06 +0300

    G done. FULL V SOLO, 0 PODSKAZOK

--- src/task/task.cpp
+++ src/task/task.cpp
@@ -7,27 +7,22 @@
 
 //--------------------------------------------------
 
-#include "d.hpp"
+#include "task.hpp"
 
 //--------------------------------------------------
 
 
 int main (void) {
 
-    int n = 0;
-    std::cin >> n;
-    Solution solution (n);
+    int max_power = 0;
+    std::cin >> max_power;
+    Solution solution (max_power);
 
-    for (int i = 0; i < n; ++i) {
-
-        int new_element = 0;
-        std::cin >> new_element;
-        solution.add_element (new_element);
-    }
 
 
     solution.pre_solve ();
     solution.solve ();
+    //solution.print_dp ();
     solution.print_result ();
 
 
@@ -37,18 +32,96 @@ int main (void) {
 
 //--------------------------------------------------
 
-Solution::Solution (int new_n):
+Solution::Solution (int max_power):
+        max_power_ (max_power),
+        answer_ (-1),
+        dp ((max_power + 1) * (max_power + 1), -1) {}
 
 //--------------------------------------------------
 
+void Solution::pre_solve (void) {
+
+    for (int i = 1; i <= max_power_; ++i) {
+
+        set_dp (i, i, 1); // count is 1 when total power == minimal power
+    }
+}
+
+
 void Solution::solve (void) {
 
-    
+    answer_ = 0;
+
+    for (int i = 1; i <= max_power_; ++i) {
+
+        answer_ += recount_dp (max_power_, i);
+    }
+}
+
+
+int Solution::recount_dp (int power, int min_number_power) {
+
+    int index = power * (max_power_ + 1) + min_number_power;
+
+
+    // уже посчитано
+    if (dp [index] != -1) return dp [index];
+
+
+    // ща пощитаем..
+    dp [index] = 0;
+
+    // перебираем возможные суффиксы
+    int suffix_power = power - min_number_power;
+    if (suffix_power < 1) { dp [index] = 0; return 0; }
+
+    for (int min_suffix_power = 2 * min_number_power; min_suffix_power <= suffix_power;
+                                                      ++min_suffix_power) {
+
+        dp [index] += recount_dp (suffix_power, min_suffix_power);
+    }
+
+
+    if (dp [index] == -1) dp [index] = 0;
+
+
+    return dp [index];
+}
+
+
+int Solution::get_dp (int power, int min_number_power) {
+
+    int index = power * (max_power_ + 1) + min_number_power;
+
+
+    return dp [index];
+}
+
+
+void Solution::set_dp (int power, int min_number_power, int value) {
+
+    int index = power * (max_power_ + 1) + min_number_power;
+
+    dp [index] = value;
 }
 
 
 void Solution::print_result (void) {
 
-    
+    std::cout << answer_;
 }
 
+
+void Solution::print_dp (void) {
+
+    for (int power = 1; power <= max_power_; ++power) {
+
+        for (int min_number_power = 1; min_number_power <= max_power_;
+                                       ++min_number_power) {
+
+            std::cout << get_dp (power, min_number_power) << " ";
+        }
+
+        std::cout << "\n";
+    }
+}
--- src/task/task.hpp
+++ src/task/task.hpp
@@ -9,14 +9,26 @@
 
 class Solution {
 
+    int max_power_;
+    long long answer_;
 
+    std::vector <int> dp;
+
+    //--------------------------------------------------
+
+    int  recount_dp (int power, int min_number_power);
+    int  get_dp     (int power, int min_number_power);
+    void set_dp     (int power, int min_number_power, int value);
 
   public:
 
-    Solution ();
+    Solution (int power);
 
+    void pre_solve (void);
     void solve (void);
     void print_result (void);
+    void print_dp (void);
+
 };
 
 
